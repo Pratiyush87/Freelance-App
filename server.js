5a7d3942d6b03b33b179ff3f7eff23b97a6f50c3
Pratiyush87 <111338545+Pratiyush87@users.noreply.github.com>
2025-07-10 16:59:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const connect = async () => {
 
 
 
-app.use(cors({ origin: "https://freelanza-frontend.vercel.app/", credentials: true }));
+app.use(cors({ origin: "https://freelanza-frontend.vercel.app", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
